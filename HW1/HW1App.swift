323c2bb07e8758526de6b42494028c8419780d3f
Janah Abu Hassan <janahabuhassan@icloud.com>
2023-09-17 22:33:13 -0400

    Initial Commit

---
+++ HW1/HW1App.swift
@@ -0,0 +1,17 @@
+//
+//  HW1App.swift
+//  HW1
+//
+//  Created by Janah Abu Hassan on 9/17/23.
+//
+
+import SwiftUI
+
+@main
+struct HW1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
